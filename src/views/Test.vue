9f8a068d0bbcc456e02be45bcda2c539772e41f1
MrThanlon <i@anclx.cc>
2021-01-14 16:19:23 +0800

    fix: CI
    Signed-off-by: MrThanlon <i@anclx.cc>

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -4,16 +4,8 @@
 </template>
 
 <script>
-import complex from 'complex.js'
-// eslint-disable-next-line import/no-webpack-loader-syntax
-import worker from 'workerize-loader!../test'
 export default {
-  name: 'Test',
-  mounted () {
-    console.debug(complex.toString())
-    const instance = worker()
-    instance.forever()
-  }
+  name: 'Test'
 }
 </script>
 
